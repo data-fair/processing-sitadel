36f15bd348883fbbf4337ecf01d2c0649628467b
WiPhil <philo102002@gmail.com>
2022-04-15 17:06:50 +0200

    feat: api request to obtain main files

--- src/download.js
+++ src/download.js
@@ -1,22 +1,19 @@
-const fs = require('fs-extra')
-const path = require('path')
-const util = require('util')
-const exec = util.promisify(require('child_process').exec)
-const config = require('config')
+// const fs = require('fs-extra')
+// const path = require('path')
+// const util = require('util')
+// const exec = util.promisify(require('child_process').exec)
+// const config = require('config')
 
 module.exports = async (pluginConfig, dir = 'data', axios, log) => {
+  const datasetId = '5a5f4f6c88ee387da4d252a3'
+  const res = await axios.get('https://www.data.gouv.fr/api/1/datasets/' + datasetId)
 
-  await log.step('Téléchargement du fichier instantané')
-  
-  const fileName = path.parse(new URL(pluginConfig.url).pathname).name + '.zip'
-  const file = `${dir}/${fileName}`
+  const ressources = res.data.resources
 
-  console.log(`download ${url} -> ${file}`)
-  const cmd = `wget -N -nv --no-check-certificate --retry-connrefused --waitretry=60 --timeout=120 -O ${fileName} "${url}"`
-
-  await exec(cmd, `${process.cwd()}/${dir}/`)
-  console.log(`unzip ${file}`)
-  await exec(`unzip -o ${file}`)
-  // remove the zip file
-  await fs.remove(fileName)
+  for (const file of ressources) {
+    if (file.type === 'main' && file.format !== 'html') {
+      console.log(file.title)
+      await axios.get(file.url)
+    }
+  }
 }
