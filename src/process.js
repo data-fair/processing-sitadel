1def53a0d4f28be4d4c13a9f73f51b7f87774be9
Nicolas Bonnel <nicolas.bonnel@gmail.com>
2022-05-17 11:59:21 +0200

    fix: regexp to get parcels

--- src/process.js
+++ src/process.js
@@ -77,11 +77,10 @@ async function getParcel (array, globalStats, processingConfig, axios, log) {
   }
   const ecart = 197
   do {
-    stringRequest += `/.*(${a.slice(0, ecart).join('|')})/`
+    stringRequest += `/${array[0].COMM}.{5}(${a.slice(0, ecart).join('|')})/`
     a = a.slice(ecart, a.length + 1)
-    if (a.length > 0) stringRequest += ' OR '
   } while (a.length > ecart)
-  if (a.length > 0) stringRequest += `/${array[0].COMM}.{5}(${a.slice(0, ecart).join('|')})/`
+  if (a.length > 0) stringRequest += ` OR /${array[0].COMM}.{5}(${a.slice(0, ecart).join('|')})/`
 
   const params = {
     qs: `code:(${stringRequest})`,
