73f7aae1e4e508d66749b514af67720f456e9bc2
BatLeDev <baptiste.guerny@gmail.com>
2024-02-15 10:29:33 +0100

    feat: change dependency processing-test-utils to @data-fair/lib

--- test/test.js
+++ test/test.js
@@ -1,14 +1,13 @@
-process.env.NODE_ENV = 'local-dev'
-const fs = require('fs-extra')
+process.env.NODE_ENV = 'test'
 const config = require('config')
-const axios = require('axios')
-const chalk = require('chalk')
-const moment = require('moment')
 const assert = require('assert').strict
 const processing = require('../')
-const path = require('path')
 
-describe('Station service processing', () => {
+describe('Sitadel processing', () => {
+  it('should expose a plugin config schema for super admins', async () => {
+    const schema = require('../plugin-config-schema.json')
+    assert.ok(schema)
+  })
   it('should expose a processing config schema for users', async () => {
     const schema = require('../processing-config-schema.json')
     assert.equal(schema.type, 'object')
@@ -17,62 +16,25 @@ describe('Station service processing', () => {
   it('should run a task', async function () {
     this.timeout(3600000)
 
-    const headers = { 'x-apiKey': config.dataFairAPIKey }
-    const axiosInstance = axios.create({
-      baseURL: config.dataFairUrl,
-      headers: headers,
-      maxContentLength: Infinity,
-      maxBodyLength: Infinity,
-      maxRedirects: 0
-    })
-
-    // customize axios errors for shorter stack traces when a request fails
-    axiosInstance.interceptors.response.use(response => response, error => {
-      if (!error.response) return Promise.reject(error)
-      delete error.response.request
-      error.response.config = { method: error.response.config.method, url: error.response.config.url, data: error.response.config.data }
-      return Promise.reject(error.response)
-    })
-
-    const pluginConfig = { urlLimit: 2000 }
-
-    const processingConfig = {
-      clearFiles: false,
-      datasetMode: 'update',
-      dataset: {
-        title: 'Sitadel - test',
-        id: 'sitadel-test-id'
+    const testsUtils = await import('@data-fair/lib/processings/tests-utils.js')
+    const context = testsUtils.context({
+      pluginConfig: { urlLimit: 2000 },
+      processingConfig: {
+        datasetMode: 'update',
+        forceUpdate: true,
+        dataset: {
+          title: 'Sitadel - test',
+          id: 'sitadel-test-id'
+        },
+        departements: ['56'],
+        processFile: 'demolir',
+        urlParcelData: {
+          href: config.parcelsUrl
+        },
+        clearFiles: false
       },
-      departements: ['13'],
-      processFile: 'amenager',
-      forceUpdate: true,
-      urlParcelData: {
-        href: config.parcelsUrl
-      },
-      tmpDir: 'data/tmp',
-      workDir: 'data/work'
-    }
-
-    const log = {
-      step: (msg) => console.log(chalk.blue.bold.underline(`[${moment().format('LTS')}] ${msg}`)),
-      error: (msg, extra) => console.log(chalk.red.bold(`[${moment().format('LTS')}] ${msg}`), extra),
-      warning: (msg, extra) => console.log(chalk.red(`[${moment().format('LTS')}] ${msg}`), extra),
-      info: (msg, extra) => console.log(chalk.blue(`[${moment().format('LTS')}] ${msg}`), extra),
-      debug: (msg, extra) => {
-        console.log(`[${moment().format('LTS')}] ${msg}`, extra)
-      }
-    }
-    const patchConfig = async (patch) => {
-      console.log('received config patch', patch)
-      Object.assign(processingConfig, patch)
-    }
-
-    // const cwd = process.cwd()
-    await fs.ensureDir(processingConfig.tmpDir)
-    await fs.ensureDir(processingConfig.workDir)
-    const tmpDir = path.resolve(processingConfig.tmpDir)
-    process.chdir(processingConfig.workDir)
-    await processing.run({ pluginConfig, processingConfig, tmpDir, axios: axiosInstance, log, patchConfig })
-    // process.chdir(cwd)
+      tmpDir: 'data'
+    }, config, false)
+    await processing.run(context)
   })
 })
